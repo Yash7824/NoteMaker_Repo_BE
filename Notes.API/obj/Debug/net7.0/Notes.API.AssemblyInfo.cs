8173538eb0d40729a484561a44ef72295b625912
Yash <yashr7824@gmail.com>
2024-02-10 15:35:21 +0530

    Docker file added

--- Notes.API/obj/Debug/net7.0/Notes.API.AssemblyInfo.cs
+++ Notes.API/obj/Debug/net7.0/Notes.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("906342e1-9e6a-4f83-9d2e-513d51cda204")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
